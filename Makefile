3bd1bfb65f0ea1f08b06d345e1f976cde54b95fc
cmoyano <cristianmoyano.mza@gmail.com>
2022-09-30 20:33:22 -0300

    add orders view

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ migrate:
 	python manage.py migrate
 
 run:
-	open http://127.0.0.1:8000/ && python manage.py runserver
+	python manage.py runserver
 
 superuser:
 	python manage.py createsuperuser
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from django.core.cache import cache
 
-# Create your views here.
+from woocommerce import API
+
+import os
+
+CACHING_TIMEOUT = 10 * 60
+
+def get_wc_api_client():
+    return API(
+        url=os.environ.get('WC_API_URL'),
+        consumer_key=os.environ.get('WC_API_CONSUMER_KEY'),
+        consumer_secret=os.environ.get('WC_API_CONSUMER_SECRET'),
+        version="wc/v3",
+    )
+
+
+
+def get_orders(client):
+    # https://woocommerce.github.io/woocommerce-rest-api-docs/?python#list-all-orders
+    # cache.delete('wc_orders')
+    if cache.get('wc_orders') is not None:
+        return cache.get('wc_orders')
+    orders = client.get("orders/?lang=es&per_page=10").json()
+    cache.set('wc_orders', orders, CACHING_TIMEOUT)
+    return orders
+
 def order_list(request):
-    return render(request, 'orders/list.html', {})
+    client = get_wc_api_client()
+    orders = get_orders(client)
+    return render(request, 'orders/list.html', {'orders': orders})
